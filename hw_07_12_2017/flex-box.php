3f5a8b682739e05c0eb972a6152dd90ea172fe66
Kovalchuk <artfire.show@gmail.com>
2018-02-03 18:14:55 +0800

    HW: from 07-12-2017

---
+++ hw_07_12_2017/flex-box.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Flex-Box</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Header</h1>
+    </div>
+    <div class="flex-box-start">
+        <div class="content-item first">
+            <p>Container 1</p>
+        </div>
+
+        <div class="content-item second">
+            <p>Container 2</p>
+        </div>
+
+        <div class="content-item third">
+            <p>Container 3</p>
+        </div>
+
+        <div class="content-item fourth">
+            <p>Container 4</p>
+        </div>
+
+        <div class="content-item fifth">
+            <p>Container 5</p>
+        </div>
+    </div>
+    <div class="footer">
+        <h3>Footer</h3>
+    </div>
+</body>
+</html>
